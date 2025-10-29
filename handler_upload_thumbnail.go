f6e5457a5d086e49de64cd4613b1a73cce0bd747
satyamdash <satyam.bn6@gmail.com>
2025-10-29 08:41:19 +0530

    encode thumbnail

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +32,55 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	tndata, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Video owner not found", err)
+		return
+	}
+
+	tn := &thumbnail{
+		data:      tndata,
+		mediaType: header.Header.Get("Content-Type"),
+	}
+
+	// videoThumbnails[video.ID] = *tn
+	fmt.Println("---------------")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
+
+	encoded_image := base64.StdEncoding.EncodeToString(tndata)
+	fmt.Println(encoded_image[:8])
+	fmt.Print("=====================================")
+
+	dataURL := "data:" + tn.mediaType + ";base64," + encoded_image
+	// url := "http://localhost:" + port + "/api/thumbnails/" + videoIDString
+
+	fmt.Println(dataURL[:8])
+	video.ThumbnailURL = &dataURL
+
+	if err := cfg.db.UpdateVideo(video); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
